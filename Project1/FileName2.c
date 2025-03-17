9471daaeea5359e5033dbd979e2292becfb6c142
hyuna <cxsixw@naver.com>
2025-03-17 17:33:10 +0900

    test

---
+++ Project1/FileName2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int result = printInfo();
+
+	return result;
+}
+
+int printInfo()
+{
+	printf("FirstApp 프로그램\n");
+	printf("작성자: 나현아\n");
+	printf("버전: 1.0\n");
+
+	return 0;
+}
